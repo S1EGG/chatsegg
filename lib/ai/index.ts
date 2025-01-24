ccb3b1ac8710736baf9f4ac43abb53a04584f529
S1EGG <tom.wangshihao@gmail.com>
2025-01-24 10:22:49 +0800

    更新项目文件：删除未使用的图片资源，更新AI相关代码

--- lib/ai/index.ts
+++ lib/ai/index.ts
@@ -1,7 +1,15 @@
-import { openai } from '@ai-sdk/openai';
+import { createOpenAI } from '@ai-sdk/openai';
 import { experimental_wrapLanguageModel as wrapLanguageModel } from 'ai';
 
 import { customMiddleware } from './custom-middleware';
+
+
+const openai = createOpenAI({
+  // custom settings, e.g.
+  compatibility: 'compatible', // strict mode, enable when using the OpenAI API
+  baseURL: process.env.OPENAI_BASE_URL, // 使用供应商的url
+  apiKey: process.env.OPENAI_API_KEY, // 使用供应商的apikey
+});
 
 export const customModel = (apiIdentifier: string) => {
   return wrapLanguageModel({
